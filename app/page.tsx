aed8c55a170f9206991a9ed90e816de02b75c668
surabhi <surabhi.bhandari1316@gmail.com>
2025-03-28 18:36:11 -0500

    blur about us

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,31 @@
+'use client';
+
 import Header from './components/Header';
 import Hero from './components/Hero';
-// import ShopSection from './components/ShopSection';
+import ShopSection from './components/ShopSection';
 import AboutSection from './components/AboutSection';
 import ReviewSection from './components/ReviewSection';
-// import StoresSection from './components/StoresSection';
 import ContactSection from './components/ContactSection';
-// import Newsletter from './components/Newsletter';
 import Footer from './components/Footer';
-import ShopSection from './components/ShopSection';
-
+import { useCartStore } from './store/cartStore';
 
 export default function Home() {
+  const isOpen = useCartStore((state) => state.isOpen);
+
   return (
-    <main>
+    <>
+      {/* Header should stay visible always */}
       <Header />
-      <Hero />
-      <ShopSection /> 
-      <AboutSection />
-      <ReviewSection />
-      {/* <StoresSection /> */}
-      {/* <Newsletter /> */}
-      <ContactSection />
-      <Footer />
-    </main>
+
+      {/* Wrap the rest of the page with blur effect when cart is open */}
+      <div className={`transition-all duration-300 ${isOpen ? 'blur-sm opacity-30 pointer-events-none' : ''}`}>
+        <Hero />
+        <ShopSection />
+        <AboutSection />
+        <ReviewSection />
+        <ContactSection />
+        <Footer />
+      </div>
+    </>
   );
 }
-
-
